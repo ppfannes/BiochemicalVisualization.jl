2d4041ece10548d0e58ed27158a1adecd43ebe03
Thomas Kemmer <thomas@binaryden.de>
2023-04-24 17:03:16 +0200

    Fix atom radii for vdW models
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/models/van_der_waals.jl
+++ src/models/van_der_waals.jl
@@ -2,7 +2,7 @@ function prepare_van_der_waals_model(
     ac::AbstractAtomContainer{T}) where {T<:Real}
 
     # todo: get vdw radii
-    spheres = map(a -> Sphere(a.r, min(a.radius, T(1.0))), atoms(ac))
+    spheres = map(a -> Sphere(a.r, max(a.radius, T(1.0))), atoms(ac))
     sphere_colors = [element_color(e) for e in atoms_df(ac).element]
 
     Representation{T}(spheres, sphere_colors)
